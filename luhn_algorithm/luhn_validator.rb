2bf1b19ec246923042ff1cf42a88fd58739a5421
Andrew Watkins <andrew.watkinz@gmail.com>
2014-11-07 08:57:53 -0700

    doners

--- luhn_algorithm/luhn_validator.rb
+++ luhn_algorithm/luhn_validator.rb
@@ -1,7 +1,33 @@
 class LuhnValidator
 
   def validate(account_number)
+    acount_parser(account_number)
+    answer
+  end
 
+  def account_parser(account_number)
+    account_array = account_number.chars.map { |i| i.to_i }.push(0).reverse
+  end
+
+  def multiply_odd_indexes(account_array)
+    multiplied = account_array.map.with_index do |ele, index|
+      if index.odd?
+        ele * 2
+      else
+        ele
+      end
+    end
+    sum_double_digits(multiplied)
+  end
+
+  def sum_double_digits(multiplied)
+    @all_nums = multiplied.map do |i|
+      i.to_s.chars.map { |i| i.to_i }.reduce(:+)
+    end
+  end
+
+  def answer
+    10 - (@all_nums.reduce(:+) % 10)
   end
 
 end
